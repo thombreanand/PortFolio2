0dd0b5531f2cfd9bd0ad6d24a627949ae7a8b733
Anand Thombre <153847243+thombreanand@users.noreply.github.com>
2025-08-09 02:18:38 +0530

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,7 @@
+public class Bill {
+    public static void main(String[] args) {
+        int price =2000;
+        
+
+    }
+}
---
+++ shoppingMall.java
@@ -0,0 +1,8 @@
+public class shoppingMall {
+    public static void main(String[] args) {
+        int a = 40000;
+        if(a>=30000){
+            System.out.println("The total bill is :"+((a)-(a/100*20)));
+        }
+    }
+}
